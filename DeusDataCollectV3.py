00e9a950c02ea46c6f672abc40bab62c3c8f96ff
Ethan Labbe <114676418+EthanLabb@users.noreply.github.com>
2022-12-26 21:47:10 -0700

    Add files via upload

--- DeusDataCollectV3.py
+++ DeusDataCollectV3.py
@@ -2,16 +2,18 @@ from bs4 import BeautifulSoup as bs
 import pandas as pd
 import requests
 import time
+import datetime 
 
 
 index=str(input('index: '))
 
 
 start = time.time()  # code timer
-i = 1
+i = 0
 numdata = 5  # number of data entries collected
 delay = 1  # seconds between each data collection
-data = []
+prices = []
+dates = []
 link = "https://www.google.com/finance/quote/{}:NASDAQ?hl=en".format(index) #change link from NAsDAQ to different stock exchange for non NASDAQ stocks
 while i < numdata:
     #code to find stock price from website HTML (Adds ~0.5 secs per itteration due to requests.get(link) line.... faster method??) faster_than_requests module
@@ -22,12 +24,22 @@ while i < numdata:
     
     #code to make put stock price in a list
     loc = text.find('$') # first character before price
-    loc2= text.find("<", loc) #last character after price
+    loc2 = text.find("<", loc) #last character after price
     price = text[loc+1:loc2]  # current stock price 
-    data.append(price)
+
+    date = str(datetime.datetime.now())
+    dates.append(date) 
+    prices.append(price)
     i = i+1
     time.sleep(delay)
         
 
-print(data)
+print(prices)
+print(dates)
 print(time.time()-start)
+
+#data_titles= ["Time", "{} Stock Price".format(index)]
+#data = list(zip(dates, prices))
+data= { 'Time': dates, '{} Stock Price'.format(index):prices}
+df= pd.DataFrame(data)
+print(df)
