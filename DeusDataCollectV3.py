43fb15ed8bb9adb6cbd52c18b6fcaa03078fa445
Ethan Labbe <114676418+EthanLabb@users.noreply.github.com>
2022-12-26 18:28:21 -0700

    Add files via upload

--- DeusDataCollectV3.py
+++ DeusDataCollectV3.py
@@ -12,19 +12,22 @@ i = 1
 numdata = 5  # number of data entries collected
 delay = 1  # seconds between each data collection
 data = []
-link = "https://www.google.com/finance/quote/{}:NASDAQ?hl=en".format(index) #change link from NADAQ to different stock exchange for non NASDAQ stocks
-page = requests.get(link)
-html_page = page.content
-soup = bs(html_page, 'html.parser')  # html garbage
-text = str(soup.find_all("div", {"class": "YMlKec fxKbKc"}))
-
-while i<numdata:
+link = "https://www.google.com/finance/quote/{}:NASDAQ?hl=en".format(index) #change link from NAsDAQ to different stock exchange for non NASDAQ stocks
+while i < numdata:
+    #code to find stock price from website HTML (Adds ~0.5 secs per itteration due to requests.get(link) line.... faster method??) faster_than_requests module
+    page = requests.get(link)
+    html_page = page.content
+    soup = bs(html_page, 'html.parser')  # html garbage
+    text = str(soup.find_all("div", {"class": "YMlKec fxKbKc"}))
+    
+    #code to make put stock price in a list
     loc = text.find('$') # first character before price
     loc2= text.find("<", loc) #last character after price
-    price = text[loc:loc2]  # current stock price (can use loc+1 if we want to remove the $ for data)
+    price = text[loc+1:loc2]  # current stock price 
     data.append(price)
     i = i+1
     time.sleep(delay)
+        
 
 print(data)
 print(time.time()-start)
